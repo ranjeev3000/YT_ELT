646c518c0f4aa059d18f2474afaba1bca05917a1
Ranjeev Tiwari <ranjeev12101@gmail.com>
2025-12-15 03:37:31 +0530

    updating changes for video stats

--- video_stats.py
+++ video_stats.py
@@ -5,6 +5,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 def get_playlist_id():
     try:
@@ -16,11 +17,41 @@ def get_playlist_id():
 
         channel_items = data["items"][0]
         channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
-        print(channel_playlistId)
+        # print(channel_playlistId)
         return channel_playlistId
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while fetching the playlist ID: {e}")
         return None
-    
+
+
+playlistId = get_playlist_id()
+
+def get_video_id(playlistId):
+    video_ids = []
+    pageToken = None
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+            pageToken = data.get("nextPageToken")
+            if not pageToken:
+                break
+        return video_ids
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching video IDs: {e}")
+        return video_ids
+
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    video_ids = get_video_id(playlistId)
