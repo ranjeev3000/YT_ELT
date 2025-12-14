7bc4ea3b56c3f61f320e7c3980902f3d04ef408d
Ranjeev Tiwari <ranjeev12101@gmail.com>
2025-12-15 04:24:08 +0530

    Saving the video details to json file ind ata folder as per curremnt timestamp

--- video_stats.py
+++ video_stats.py
@@ -1,5 +1,6 @@
 import requests, json, os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path="./.env")
 
@@ -94,7 +95,17 @@ def extract_video_data(video_ids):
         return extracted_data
     except requests.exceptions.RequestException as e:
         raise e
+    
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+    print(file_path)
+
+    with open(file_path, 'w', encoding='utf-8') as json_file:
+        json.dump(extracted_data, json_file, indent=4, ensure_ascii=False)
+
+
 if __name__ == "__main__":
     playlistId = get_playlist_id()
     video_ids = get_video_id(playlistId)
-    print(extract_video_data(video_ids))
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
